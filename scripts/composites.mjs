93118d76fc97e32e6e2e67135ff52e432597eddd
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-08-18 16:33:50 -0400

    Adding for main

--- scripts/composites.mjs
+++ scripts/composites.mjs
@@ -5,7 +5,7 @@ import { Ed25519Provider } from "key-did-provider-ed25519";
 import { getResolver } from "key-did-resolver";
 import { fromString } from "uint8arrays/from-string";
 
-const ceramic = new CeramicClient("https://ceramic-temp.hirenodes.io");
+const ceramic = new CeramicClient("http://localhost:7007");
 
 /**
  * @param {Ora} spinner - to provide progress status.
@@ -13,7 +13,62 @@ const ceramic = new CeramicClient("https://ceramic-temp.hirenodes.io");
  */
 export const writeComposite = async (spinner) => {
   await authenticate()
-  spinner.succeed("admin authenticated successfully");
+  const profileComposite = await createComposite(
+    ceramic,
+    "./composites/00-basicProfile.graphql"
+  );
+
+  const postsSchema = readFileSync("./composites/01-posts.graphql", {
+    encoding: "utf-8",
+  }).replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const postsComposite = await Composite.create({
+    ceramic,
+    schema: postsSchema,
+  });
+
+  const followingSchema = readFileSync("./composites/02-following.graphql", {
+    encoding: "utf-8",
+  }).replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const followingComposite = await Composite.create({
+    ceramic,
+    schema: followingSchema,
+  });
+
+  const postProfileSchema = readFileSync(
+    "./composites/03-posts_profile.graphql",
+    {
+      encoding: "utf-8",
+    }
+  )
+    .replace("$POSTS_ID", postsComposite.modelIDs[1])
+    .replace("$PROFILE_ID", profileComposite.modelIDs[0]);
+
+  const postsProfileComposite = await Composite.create({
+    ceramic,
+    schema: postProfileSchema,
+  });
+
+  const composite = Composite.from([
+    profileComposite,
+    postsComposite,
+    followingComposite,
+    postsProfileComposite,
+  ]);
+
+  await writeEncodedComposite(composite, "./src/__generated__/definition.json");
+  spinner.info('creating composite for runtime usage')
+  await writeEncodedCompositeRuntime(
+    ceramic,
+    "./src/__generated__/definition.json",
+    "./src/__generated__/definition.js"
+  );
+  spinner.info('deploying composite')
+  const deployComposite = await readEncodedComposite(ceramic, './src/__generated__/definition.json')
+
+  await deployComposite.startIndexingOn(ceramic)
+  spinner.succeed("composite deployed & ready for use");
 }
 
 /**
@@ -21,7 +76,7 @@ export const writeComposite = async (spinner) => {
  * @return {Promise<void>} - return void when DID is authenticated.
  */
 const authenticate = async () => {
-  const seed = process.env.SEED
+  const seed = readFileSync('./admin_seed.txt')
   const key = fromString(
     seed,
     "base16"
